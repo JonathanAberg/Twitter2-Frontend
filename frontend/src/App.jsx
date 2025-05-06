a910e48c598cdc7792c43daa66cf9196805f6ad8
Amir Ahmadi <amir23060@gmail.com>
2025-05-06 15:18:37 +0200

    fixed the misspeling in routers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { About } from './components/About';
-import { Register } from './components/Register';
-
-import './styles/general.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import "./styles/general.css";
+import Inlogning from "./inlogning/inlogning";
+import Registering from "./inlogning/registrering";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-        <Route path="/" element={<Layout />}>
-        <Route path="/login" index element={<Login />}/>
-        <Route path="/register" element={<Register />}/>
-        <Route path="/home" element={<Home />}/>
-        <Route path="/about" element={<About />}/>
-        </Route>
+          <Route path="/" element={<Layout />}>
+            <Route path="/login" element={<Inlogning />} />
+            <Route path="/register" element={<Registering />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
