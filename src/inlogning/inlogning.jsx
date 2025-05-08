a69b82678a4463522494809fb4b8b035afe62b22
Amir Ahmadi <amir23060@gmail.com>
2025-05-08 11:22:13 +0200

    added the frontend files from the old code

---
+++ src/inlogning/inlogning.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+const Inlogning = ({ data }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = data?.find(
+      (user) => user.email === email && user.password === password
+    );
+
+    if (user) {
+      alert("correct");
+    } else {
+      alert("incorrect");
+    }
+  };
+  return (
+    <>
+      <h1>Log in</h1>
+      <form onSubmit={handleSubmit}>
+        <p>
+          email{" "}
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </p>
+        <p>
+          password{" "}
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </p>
+        <input type="submit" value={"log in"} />
+      </form>
+    </>
+  );
+};
+export default Inlogning;
