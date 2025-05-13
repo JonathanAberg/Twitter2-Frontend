d537f6fdc23e4a83228aff3a05585a819dcf0672
Kleamy8 <remanrada8@gmail.com>
2025-05-13 18:12:57 +0200

    Add my local backend files

---
+++ backend/controllers/userController.js
@@ -0,0 +1,150 @@
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET || 'nyckel', {
+        expiresIn: '30d'
+    });
+};
+
+exports.registerUser = async (req, res) => {
+    try {
+        const { name, email, password, nickname, about, occupation, hometown, website } = req.body;
+
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ message: 'Användaren finns redan'});
+        }
+
+        const user = await User.create({
+            name,
+            email,
+            password,
+            nickname: nickname || name,
+            about,
+            occupation,
+            hometown,
+            website
+        });
+
+        if (user) {
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                nickname: user.nickname,
+                token: generateToken(user._id)
+            });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
+
+exports.loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+       if (user && (await user.matchPassword(password))) {
+          res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            nickname: user.nickname,
+            token: generateToken(user._id)
+          });
+        } else {
+            res.status(401).json({ message: 'Felaktigt email eller lösenord'});
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
+
+exports.getUsers = async (req, res) => {
+    try {
+        const users = await User.find({}).select('-password');
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
+
+exports.getUserProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        .select ('-password');
+        
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).json({ message: 'Användare hittades inte' });
+        }
+    } catch(error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
+
+exports.followUser = async (req, res) => {
+    try {
+        if (req.user._id.toString() === req.params.id) {
+            return res.status(400).json({ message: 'Du kan inte följa dig själv'})
+        }
+
+        const userToFollow = await User.findById(req.params.id);
+        if (!userToFollow) {
+            return res.status(400).json({ message: 'Användare hittades inte'});
+        }
+
+        const currentUser = await User.findById(req.user._id);
+
+        if (currentUser.following.includes(req.params.id)) {
+            return res.status(400).json({ message: 'Du följer redan denna användare'});
+        }
+
+        await User.findByIdAndUpdate(req.user._id, {
+            $push: { following: req.params.id }
+        });
+
+        await User.findByIdAndUpdate(req.params.id, {
+            $push: { followers: req.user._id}
+        });
+
+        res.json({ message: 'Användare följs nu' });
+    } catch(error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
+
+exports.unfollowUser = async (req, res) => {
+    try {
+        if (req.user._id.toString() === req.params.id) {
+            return res.status(400).json({ message: 'Du kan inte sluta följa dig själv' });
+        }
+
+        const userToUnfollow = await User.findById(req.params.id);
+        if (!userToUnfollow) {
+            return res.status(400).json({ message: 'Användare hittades inte' });
+        }
+
+        const currentUser = await User.findById(req.user._id);
+
+        if (!currentUser.following.includes(req.params.id)) {
+            return res.status(400).json({ message: 'Du följer inte denna användare' });
+        }
+
+        await User.findByIdAndUpdate(req.user._id, {
+            $pull: { following: req.params.id }
+        });
+
+        await User.findByIdAndUpdate(req.params.id, {
+            $pull: { followers: req.user._id }
+        });
+
+        res.json({ message: 'Du följer inte längre denna användare' });
+    } catch(error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+}
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { 
+    registerUser, 
+    loginUser, 
+    getUsers, 
+    getUserProfile, 
+    followUser, 
+    unfollowUser 
+
+} = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+router.post('/', registerUser);
+router.post('/login', loginUser);
+
+router.get('/', protect, getUsers);
+router.get('/:id', protect, getUserProfile);
+router.post('/:id/follow', protect, unfollowUser);
+router.post('/:id/unfollow', protect, unfollowUser);
+
+module.exports = router;
