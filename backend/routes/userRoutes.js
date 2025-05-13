724b1f1096909bd1bcffea2dddef4389a63e3848
Kleamy8 <remanrada8@gmail.com>
2025-05-13 18:49:34 +0200

    Fix follow route to use correct controller function

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,23 +1,22 @@
-const express = require('express');
-const { 
-    registerUser, 
-    loginUser, 
-    getUsers, 
-    getUserProfile, 
-    followUser, 
-    unfollowUser 
-
-} = require('../controllers/userController');
-const { protect } = require('../middleware/authMiddleware');
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  getUsers,
+  getUserProfile,
+  followUser,
+  unfollowUser,
+} = require("../controllers/userController");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.post('/', registerUser);
-router.post('/login', loginUser);
+router.post("/", registerUser);
+router.post("/login", loginUser);
 
-router.get('/', protect, getUsers);
-router.get('/:id', protect, getUserProfile);
-router.post('/:id/follow', protect, unfollowUser);
-router.post('/:id/unfollow', protect, unfollowUser);
+router.get("/", protect, getUsers);
+router.get("/:id", protect, getUserProfile);
+router.post("/:id/follow", protect, followUser);
+router.post("/:id/unfollow", protect, unfollowUser);
 
 module.exports = router;
