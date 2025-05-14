a6de6028ad945dc3c64214b47ff53c708ad93a39
Kleamy8 <remanrada8@gmail.com>
2025-05-14 11:23:50 +0200

    Fixed type error in authMiddleware (startsWidth -> startsWith)

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,27 +1,35 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/userModel');
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
 
 const protect = async (req, res, next) => {
-    let token;
+  let token;
 
-    if (req.headers.authorization && req.headers.authorization.startsWidth('Bearer')) {
-        try {
-            token = req.headers.authorization.split(' ')[1];
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
 
-            const decoded = jwt.verify(token, process.env.JWT_SECRET || 'hemlig_nyckel');
+      const decoded = jwt.verify(
+        token,
+        process.env.JWT_SECRET || "hemlig_nyckel"
+      );
 
-            req.user = await User.findById(decoded.id).select('-password');
+      req.user = await User.findById(decoded.id).select("-password");
 
-            next ();
-        } catch (error) {
-            console.error(error);
-            res.status(401).json({ message: 'Inte auktoriserad, token misslyckades' });
-        }
+      next();
+    } catch (error) {
+      console.error(error);
+      res
+        .status(401)
+        .json({ message: "Inte auktoriserad, token misslyckades" });
     }
+  }
 
-    if (!token) {
-        res.status(401).json({ message: 'Inte auktoriserad, ingen token' });
-    }
+  if (!token) {
+    res.status(401).json({ message: "Inte auktoriserad, ingen token" });
+  }
 };
 
 module.exports = { protect };
