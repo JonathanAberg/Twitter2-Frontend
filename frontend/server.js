3e32d3660b2522929b284c123884f21865585ca8
Kleamy8 <remanrada8@gmail.com>
2025-05-15 12:18:36 +0200

    .

--- frontend/server.js
+++ frontend/server.js
@@ -1,24 +1,46 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
+const tweetRoutes = require("./routes/tweetRoutes");
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/twitter-clone', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log('Ansluten till MongoDB'))
-.catch(err => console.error('Kunde inte ansluta till MongoDB', err));
+console.log("Environment variables:", {
+  PORT: process.env.PORT,
+  MONGODB_URI: process.env.MONGODB_URI ? "URI exists" : "URI missing",
+});
 
+mongoose
+  .connect(
+    "mongodb+srv://remanrada8:1RIWudunt0PZsMhz@twitter.zc9flxu.mongodb.net/?retryWrites=true&w=majority&appName=Twitter",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Ansluten till MongoDB!"))
+  .catch((err) => console.error("Kunde inte ansluta till MongoDB", err));
 
+app.use("/user", userRoutes);
+app.use("/api/tweets", tweetRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Twitter API is running');
+app.get("/", (req, res) => {
+  res.send("Twitter API is running");
 });
 
 app.listen(PORT, () => {
-    console.log('Server är igång på port ${PORT}');
+  console.log(`Server är igång på port ${PORT}`);
 });
