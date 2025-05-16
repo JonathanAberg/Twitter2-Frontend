af9bcd481f5a79ae5337ff486b188ec5b6b94515
Kleamy8 <remanrada8@gmail.com>
2025-05-16 13:48:53 +0200

    .

--- backend/test.config.js
+++ backend/test.config.js
@@ -1,3 +1,6 @@
+process.env.NODE_ENV = "test";
+process.env.JWT_SECRET = "test-jwt-secret";
+
 module.exports = {
   testEnvironment: "node",
   testMatch: ["**/_tests_/**/*.js", "**/?(*.)+(spec|test).js"],
