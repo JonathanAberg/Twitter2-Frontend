584deadd32a28f73a414f1b0369cbc60917f1dc2
Kleamy8 <remanrada8@gmail.com>
2025-05-16 10:31:44 +0200

    .

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ app.get("/", (req, res) => {
   res.send("Twitter API is running");
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server är igång på port ${PORT}`);
 });
