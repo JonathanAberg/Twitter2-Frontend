4dbe899f04d714bf9195e8934415bad19197836b
Amir Ahmadi <amir23060@gmail.com>
2025-05-20 19:04:38 +0200

    changed url to the new one

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -37,11 +37,14 @@ export function Home() {
           return;
         }
 
-        const response = await fetch(`http://localhost:5001/user/${userId}`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
+        const response = await fetch(
+          `http://localhost:5001/api/users/${userId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
 
         if (!response.ok) {
           throw new Error(`Failed to fetch user data: ${response.status}`);
