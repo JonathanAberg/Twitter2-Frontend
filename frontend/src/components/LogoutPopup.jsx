586801f7e3615af5563f2f58db464d033a30628d
Amir Ahmadi <amir23060@gmail.com>
2025-05-26 18:36:04 +0200

    added naviation

--- frontend/src/components/LogoutPopup.jsx
+++ frontend/src/components/LogoutPopup.jsx
@@ -1,21 +1,26 @@
-import TwitterLogo from '../assets/twitter-logo.svg'
-import '../styles/logoutPopup.css'
+import { useNavigate } from "react-router-dom";
+import TwitterLogo from "../assets/twitter-logo.svg";
+import "../styles/logoutPopup.css";
 
 export function LogoutPopup({ onCancel }) {
-     return (
-        <div className="logout-wrapper">
-            <div className="logo-container">
-                <img src={TwitterLogo} alt="Twitter Logo" />
-                <h3>Log out of Twitter</h3>
-            </div>
-            <div className="btn-wrapper">
-                <button className="logout-btn">
-                Log out
-                </button >
-                <button onClick={onCancel} className="cancel-btn">
-                Cancel    
-                </button>
-            </div>
-</div>
-    )
+  const navigate = useNavigate();
+  return (
+    <div className="logout-wrapper">
+      <div className="logo-container">
+        <img src={TwitterLogo} alt="Twitter Logo" />
+        <h3>Log out of Twitter</h3>
+      </div>
+      <div className="btn-wrapper">
+        <button
+          className="logout-btn"
+          onClick={() => navigate("/", { replace: true })}
+        >
+          Log out
+        </button>
+        <button onClick={onCancel} className="cancel-btn">
+          Cancel
+        </button>
+      </div>
+    </div>
+  );
 }
