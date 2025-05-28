5126df612146e07ea4b24baa8cb0dd50cc516d7e
Linus <sunkan.sundqvist@gmail.com>
2025-05-28 22:02:20 +0200

    Fix: Corrected cod to reflect changes to layout, making logout-wrapper get displayed properly when using the footer icon. Style: Add cursor: pointer to both buttons in the logout-wrapper. Add 1rem margin-top to Aside to better align with the other elements. Code cleanup of several redundant lines

--- frontend/src/components/ShowCurrentProfile.jsx
+++ frontend/src/components/ShowCurrentProfile.jsx
@@ -7,20 +7,18 @@ import { UserContext } from './UserContext';
 export function ShowCurrentProfile({ children }) {
 const { user } = useContext(UserContext);
 
-console.log("User in ShowCurrentProfile:", user);
-
   if (!user) return null;
     
     return (
-                    <div className="profileinfo-wrapper">
-                    <div className="user-pic">
-                        <img src={profilePlaceholder} alt="Profile Picture" />
-                    </div>
-                    <div className="profileinfo-text">
-                    <strong>{user.name}</strong>
-                    <p className="user-desc">@{user.nickname}</p>
-                    </div>
-                    {children}
-                    </div>
-                    )
+        <div className="profileinfo-wrapper">
+        <div className="user-pic">
+        <img src={profilePlaceholder} alt="Profile Picture" />
+        </div>
+        <div className="profileinfo-text">
+        <strong>{user.name}</strong>
+        <p className="user-desc">@{user.nickname}</p>
+        </div>
+        {children}
+        </div>
+        )
 }
